c3d882f5e63d60538f0c40b64bbecbaad119faa6
Sebastian Safari <ssebs@ymail.com>
2018-09-25 16:42:17 -0700

    updated proceduer

--- bin/_build.py
+++ bin/_build.py
@@ -5,7 +5,18 @@
 
 ##
 #   This file should be ran by the ssebsMS.py file
-## 
+##
+
+## Procedure on how pages will be built:
+# 0) [ ] Check / read config file
+# 1) [ ] list dirs
+# 2) [ ] check for md files & make sure they match config
+# 3) [ ] loop through page md files and get file based configs
+#   4) [ ] make list of page objects & set their settings
+#   5) [ ] read contents of Markdown
+#   6) [ ] generate html per page in public/ dir
+# 7) Test to make sure pages are there
+
 
 # test definition for import testing
 def build_test(my_arg):
