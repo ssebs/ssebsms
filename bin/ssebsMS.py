aac75e66c06ceebbc74e00fcafed4eb5783a3d44
Sebastian <ssebs@ymail.com>
2018-09-24 18:19:03 -0700

    started on main python file

---
+++ bin/ssebsMS.py
@@ -0,0 +1,21 @@
+###
+#   ssebsMS.py - ssebsMS cli utility
+###
+
+# help output below (future commands to support)
+help_output = '''
+ssebsMS.py <CMD> [arg]
+
+Possible CMD's:
+    init [-o <FOLDERNAME]           <- initialize a new ssebsMS website
+    build                           <- build current website
+    clean                           <- clean generated files
+    help                            <- output this help page
+    
+Possible args:
+    -o <FOLDERNAME> <- Output foldername (when initializing)
+    -h              <- outpput this help page
+    -v              <- Verbose output
+    -q              <- Quiet output
+'''
+
